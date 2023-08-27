c80003ca33653e71917bc813aa08a573a2e428cd
Gigii55 <gigibernadelli001@gmail.com>
2023-08-27 17:36:36 -0300

    atualizacao2

--- index.php
+++ index.php
@@ -2,87 +2,100 @@
 <html lang="en">
 
 <head>
-    <<?php include_once 'includes/metadados.php' ?> <title>Document</title>
+  <link href="bibliotecas/bootstrap-5.3.0/css/bootstrap.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/index.css" media="screen" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+  <title>Document</title>
 </head>
 
 <body>
 
-    <?php
-    include_once 'navbar.php';
-    ?>
+  <?php
+  include_once 'navbar.php';
+  ?>
 
 
-    <div class="lancamento-livros">
-        <div id="new" class="livro-novo">
+  <div class="lancamento-livros">
+    <div id="new" class="livro-novo">
 
-            <img class="livronovo-img" src="imgs/livronovo.png" />
-            <div class="lancamento">
-                <h1>NOVO LANÇAMENTO! </h1>
-                <h2 class="introducao-novo"> Dois físicos rivais colidem em meio a um turbilhão de disputas acadêmicas e
-                    caóticos namoros de mentira nesta nova comédia romântica da autora de A hipótese do amor.
-                    "Ali Hazelwood é uma verdadeira potência dos romances." – Christina Lauren, autora de Imperfeitos
-                </h2>
-                <button id="esconder" class="botao">OK</button>
-            </div>
-        </div>
-        <div class="p1">
-            <p> Mais vendidos </p>
-        </div>
-
-        <div class="baixo-livros">
-            <div class="linha">
-                <div class="livro">
-
-
-                    <div class="img-livro">
-                        <img src="imgs/principe.png">
-                    </div>
-                    <div class="detalhes-livro">
-                        <h3 class="avaliacoes2">3.863 </h3>
-                        <h3 class="autora2">Taylor Jenkins Reid </h3>
-                        <h2 class="introducao2"> O Pequeno Príncipe é uma obra prima que conduz o leitor a uma viagem de
-                            descobertas pela essência humana.Este clássico atemporal de Antoine de Saint-Exupéry narra a
-                            história sobre o encontro de um aviador e um menino com “cabelos da cor do ouro”. À medida
-                            que o principezinho conta ao aviador sobre sua rotina no asteroide B 612, sua rosa única, o
-                            perigo dos baobás, sua raposa e os moradores de outros planetas, um novo
-                            olhar sobre a vida e o mundo é revelado. </h2>
-                    </div>
-                </div>
-                <div class="livro">
-
-
-                    <div class="img-livro">
-                        <img src="imgs/7maridos.png">
-                    </div>
-                    <div class="detalhes-livro">
-                        <h3 class="avaliacoes">2.931 </h3>
-                        <h3 class="autora"> Antoine de Saint-Exupéry </h3>
-                        <h2 class="introducao"> O Pequeno Príncipe é uma obra prima que conduz o leitor a uma viagem de
-                            descobertas pela essência humana.Este clássico atemporal de Antoine de Saint-Exupéry narra a
-                            história sobre o encontro de um aviador e um menino com “cabelos da cor do ouro”. À medida que o
-                            principezinho conta ao aviador sobre sua rotina no asteroide B 612, sua rosa única, o perigo dos
-                            baobás, sua raposa e os moradores de outros planetas, um novo
-                            olhar sobre a vida e o mundo é revelado. </h2>
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
+      <img class="livronovo-img" src="imgs/livronovo.png" />
+      <div class="lancamento">
+        <h1>NOVO LANÇAMENTO! </h1>
+        <h2 class="introducao-novo"> Dois físicos rivais colidem em meio a um turbilhão de disputas acadêmicas e
+          caóticos namoros de mentira nesta nova comédia romântica da autora de A hipótese do amor.
+          "Ali Hazelwood é uma verdadeira potência dos romances." – Christina Lauren, autora de Imperfeitos
+        </h2>
+        <button id="esconder" class="botao">OK</button>
+      </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="bibliotecas/bootstrap-5.3.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    <div class="p1">
+      <p> Mais vendidos </p>
+    </div>
 
-    <script>
-        $(document).ready(function() {
+    <div class="baixo-livros">
+      <div class="linha">
+        
+        <div class="livro">
+          <div class="nome">O pequeno príncipe </div>
+          <div class="img-livro">
+          <img class="img1"src="imgs/pequenoprincipe.jpg">
+          
+          </div>
+          <div class="detalhes-livro">
+          <img class="estrelas"src="imgs/estrelas.png">
+          <h3 class="avaliacoes2">3.863 </h3>
+        <h3 class="autora2">Taylor Jenkins Reid </h3>
+        <h2 class="introducao2"> O Pequeno Príncipe é uma obra prima que conduz o leitor a uma viagem de
+          descobertas pela essência humana.Este clássico atemporal de Antoine de Saint-Exupéry narra a
+          história sobre o encontro de um aviador e um menino com “cabelos da cor do ouro”. À medida
+          que o principezinho conta ao aviador sobre sua rotina no asteroide B 612, sua rosa única, o
+          perigo dos baobás, sua raposa e os moradores de outros planetas, um novo
+          olhar sobre a vida e o mundo é revelado. </h2>
+          </div>
+        </div>
+        <div class="livro">
 
-            $("#esconder").click(function() {
-                $("#new").hide();
-            });
-        });
-    </script>
-    <?php include_once 'includes/scripts.php' ?>
+
+          <div class="img-livro">
+          </div>
+          <img class="estrelas"src="imgs/estrelas.png">
+          <img class="img2"src="imgs/evelynhugo.jpg">
+          <div class="detalhes-livro">
+          <h3 class="avaliacoes">2.931 </h3>
+        <h3 class="autora"> Antoine de Saint-Exupéry </h3>
+        <h2 class="introducao"> O Pequeno Príncipe é uma obra prima que conduz o leitor a uma viagem de
+          descobertas pela essência humana.Este clássico atemporal de Antoine de Saint-Exupéry narra a
+          história sobre o encontro de um aviador e um menino com “cabelos da cor do ouro”. À medida que o
+          principezinho conta ao aviador sobre sua rotina no asteroide B 612, sua rosa única, o perigo dos
+          baobás, sua raposa e os moradores de outros planetas, um novo
+          olhar sobre a vida e o mundo é revelado. </h2>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+  <script src="bibliotecas/bootstrap-5.3.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+  <script>
+    $(document).ready(function () {
+
+      $("#esconder").click(function () {
+        $("#new").hide();
+      });
+    });
+  </script>
 </body>
 
 </html>
--- navbar.php
+++ navbar.php
@@ -2,7 +2,10 @@
 <html lang="en">
 
 <head>
-    <?php include_once 'includes/metadados.php' ?>
+    <link href="bibliotecas/bootstrap-5.3.0/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./css/nav.css" media="screen" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 
@@ -53,11 +56,15 @@
         </div>
 
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="bibliotecas/bootstrap-5.3.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
-    <?php include_once 'includes/scripts.php' ?>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                    crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+                    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+                    crossorigin="anonymous"></script>
+                <script src="bibliotecas/bootstrap-5.3.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
+
