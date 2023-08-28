9b4e096d9f4d3b0e4eaf0fa8c7dc93e8e9b9c508
Gigii55 <gigibernadelli001@gmail.com>
2023-08-27 21:46:05 -0300

    atualizacao3

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
   ?>
 
 
-  <div class="lancamento-livros">
-    <div id="new" class="livro-novo">
+  <div id="new" class="lancamento-livros">
+    <div  class="livro-novo">
 
       <img class="livronovo-img" src="imgs/livronovo.png" />
       <div class="lancamento">
@@ -29,8 +29,9 @@
           caóticos namoros de mentira nesta nova comédia romântica da autora de A hipótese do amor.
           "Ali Hazelwood é uma verdadeira potência dos romances." – Christina Lauren, autora de Imperfeitos
         </h2>
-        <button id="esconder" class="botao">OK</button>
+        <button id="esconder" class="btn btn-outline-light mt-4 position-absolute bottom-20 end-50">OK</button>
       </div>
+  </div>
     </div>
     <div class="p1">
       <p> Mais vendidos </p>
@@ -38,48 +39,50 @@
 
     <div class="baixo-livros">
       <div class="linha">
-        
+
         <div class="livro">
           <div class="nome">O pequeno príncipe </div>
           <div class="img-livro">
-          <img class="img1"src="imgs/pequenoprincipe.jpg">
-          
+            <img class="img1" src="imgs/pequenoprincipe.jpg">
+
           </div>
           <div class="detalhes-livro">
-          <img class="estrelas"src="imgs/estrelas.png">
-          <h3 class="avaliacoes2">3.863 </h3>
-        <h3 class="autora2">Taylor Jenkins Reid </h3>
-        <h2 class="introducao2"> O Pequeno Príncipe é uma obra prima que conduz o leitor a uma viagem de
-          descobertas pela essência humana.Este clássico atemporal de Antoine de Saint-Exupéry narra a
-          história sobre o encontro de um aviador e um menino com “cabelos da cor do ouro”. À medida
-          que o principezinho conta ao aviador sobre sua rotina no asteroide B 612, sua rosa única, o
-          perigo dos baobás, sua raposa e os moradores de outros planetas, um novo
-          olhar sobre a vida e o mundo é revelado. </h2>
+            <img class="estrelas" src="imgs/estrelas.png">
+            <h3 class="avaliacoes">3.863 </h3>
+            <h3 class="autora2">Taylor Jenkins Reid </h3>
+            <h2 class="introducao2"> O Pequeno Príncipe é uma obra prima que conduz o leitor a uma viagem de
+              descobertas pela essência humana.Este clássico atemporal de Antoine de Saint-Exupéry narra a
+              história sobre o encontro de um aviador e um menino com “cabelos da cor do ouro”. À medida
+              que o principezinho conta ao aviador sobre sua rotina no asteroide B 612, sua rosa única, o
+              perigo dos baobás, sua raposa e os moradores de outros planetas, um novo
+              olhar sobre a vida e o mundo é revelado. </h2>
           </div>
         </div>
+
+
+
         <div class="livro">
-
-
+          <div class="nome2">Os sete maridos de Evelyn Hugo</div>
           <div class="img-livro">
+            <img class="img2" src="imgs/evelynhugo.jpg">
           </div>
-          <img class="estrelas"src="imgs/estrelas.png">
-          <img class="img2"src="imgs/evelynhugo.jpg">
           <div class="detalhes-livro">
-          <h3 class="avaliacoes">2.931 </h3>
-        <h3 class="autora"> Antoine de Saint-Exupéry </h3>
-        <h2 class="introducao"> O Pequeno Príncipe é uma obra prima que conduz o leitor a uma viagem de
-          descobertas pela essência humana.Este clássico atemporal de Antoine de Saint-Exupéry narra a
-          história sobre o encontro de um aviador e um menino com “cabelos da cor do ouro”. À medida que o
-          principezinho conta ao aviador sobre sua rotina no asteroide B 612, sua rosa única, o perigo dos
-          baobás, sua raposa e os moradores de outros planetas, um novo
-          olhar sobre a vida e o mundo é revelado. </h2>
+          <img class="estrelas2"src="imgs/estrelas.png">
+          <h3 class="avaliacoes2">2.931 </h3>
+            <h3 class="autora"> Antoine de Saint-Exupéry </h3>
+            <h2 class="introducao2"> O Pequeno Príncipe é uma obra prima que conduz o leitor a uma viagem de
+              descobertas pela essência humana.Este clássico atemporal de Antoine de Saint-Exupéry narra a
+              história sobre o encontro de um aviador e um menino com “cabelos da cor do ouro”. À medida que o
+              principezinho conta ao aviador sobre sua rotina no asteroide B 612, sua rosa única, o perigo dos
+              baobás, sua raposa e os moradores de outros planetas, um novo
+              olhar sobre a vida e o mundo é revelado. </h2>
           </div>
         </div>
       </div>
 
 
     </div>
-  </div>
+ 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
     crossorigin="anonymous"></script>
--- sagas.php
+++ sagas.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+<link rel="stylesheet" type="text/css" href="css/sagas.css" media="screen" />
     <?php include_once 'includes/metadados.php' ?>
     <title>Sagas</title>
 </head>
@@ -10,17 +11,42 @@
     <?php
     include("navbar.php");
     ?>
-    <h1>Sagas</h1>
-
-    <div class="container">
-        <a href="pjo.php" class="button">Percy <br> Jackson</a>
-        <a href="senhoraneis.php" class="button">Senhor dos <br> Anéis</a>
-        <a href="./sagas/jogosvorazes.php" class="button">Jogos <br> Vorazes</a>
-        <a href="./sagas/narnia.php" class="button">As Crônicas de <br>Nárnia</a>
-    </div>
+   
 
     <?php include_once 'includes/scripts.php' ?>
+<div class="botoes">
 
+<a href="autores.php" class="grupo-link"> Senhor dos Anéis</a>
+            <a href="sagas.php" class="grupo-link">As Crônicas de Nárnia</a>
+            <a href="box.php" class="grupo-link">PErcy Jackson</a>
+            <a href="editoras.php" class="grupo-link">Jogos Vorazes</a>
+</div>
+
+    <style>
+ *{
+  margin:0;
+  padding: 0;
+  box-sizing: 0;
+   }
+   body{
+    height: 100vh;
+    width: 100vw;
+    background-image: url('./imgs/plano.png');
+   }
+   .botoes {
+          background-color:#1A4A65;
+            width: 1010px;
+            height: 180px;
+            margin-left: 270px;
+            margin-top: 200px;
+            display: flex; /* Para usar espaçamento interno (padding) entre os botões */
+            gap: 10px; /* Espaçamento entre os botões */
+            align-items: flex-start; /* Alinhar os botões à esquerda */
+            padding: 20px; /* Adicionar espaçamento interno para espaçamento entre os botões */
+            border-radius: 30px;
+        }
+    
+   </style>
 </body>
 
 </html>
