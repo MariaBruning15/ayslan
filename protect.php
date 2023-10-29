1f4eb6933a6ef274f1b56b18f753800a6bc21e1d
MariaBruning15 <mariaeduardabruning@gmail.com>
2023-10-29 18:18:52 -0300

    alterações

--- protect.php
+++ protect.php
@@ -19,7 +19,7 @@
         <div class="justify content-center d-flex flex-column" style="width:90%; margin-left:5%; margin-right:5%">
             <label class="mx-auto form-control is-invalid fs-5" for="floatingInputInvalid"> Você não pode acessar esta página porque não está logado.</label>
             <br>
-            <a href="login.php" style="margin:auto;background-color: #1A4A65" class="btn fs-4 text-white" role="button">Entrar</a></p>
+            <a href="login.php" style="margin:auto;background-color: #1A4A65" class="btn fs-4 text-white btn-lg" role="button">Entrar</a></p>
         </div>
 
 
