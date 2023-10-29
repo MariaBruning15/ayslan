253e67d19c076af56070ebd1beb794c485053164
MariaBruning15 <mariaeduardabruning@gmail.com>
2023-10-29 17:25:46 -0300

    painel e protect

--- painel.php
+++ painel.php
@@ -14,12 +14,44 @@ include('protect.php');
 </head>
 
 <body>
-    Bem vindo ao Painel, <?php echo $_SESSION['nome']; ?>.
+    <div class="container p-5 custom-background-color my-5 rounded-2">
+        <h1 class="título custom-color text-center my-2 mb-4 fs-1 text fw-bold">Painel</h1>
+        <p class="p text-center fs-5 text"> Bem vindo ao Painel, <?php echo $_SESSION['nome'] ?>.</p>
 
-    <p>
-        <a href="logout.php">Sair</a>
-        <a href="index.php">Voltar para o site </a>
-    </p>
+
+
+        <div class=" container text-center">
+            <form action="validalogin.php" method="post">
+                <style>
+                    .custom-color {
+                        color: #3A478E;
+                    }
+
+                    .custom-color2 {
+                        color: #3A7A8E;
+                    }
+
+                    .custom-background-color {
+                        background-color: #B1CCD4;
+                    }
+
+                    .custom-background-color2 {
+                        background-color: #3A7A8E;
+                    }
+
+                    .custom-background-color3 {
+                        background-color: #3A478E;
+                    }
+                </style>
+
+        </div>
+
+        <div class="row align-items-center justify-content-center">
+
+                <a href="logout.php" class="btn rounded-2 custom-background-color3 my-2 text-white btn-lg mx-3" style="width:150px" role="button">Sair</a>
+                <a href="index.php" class="btn rounded-2 custom-background-color3 my-2 text-white btn-lg mx-3" style="width:250px" role=" button">Voltar para o site </a>
+
+            </div>
 </body>
 
 </html>
--- protect.php
+++ protect.php
@@ -1,12 +1,31 @@
-<?php
+<head>
+    <?php include_once 'includes/metadados.php' ?>
+    <title>Protect</title>
+</head>
 
-if(!isset($_SESSION)) {
-    session_start();
-}
-
-if(!isset($_SESSION['id'])) {
-    die("Você não pode acessar esta página porque não está logado.<p><a href=\"login.php\">Entrar</a></p>");
-}
+<body>
 
 
-?>
+    <?php
+
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+
+    if (!isset($_SESSION['id'])) {
+
+    ?>
+        <br>
+        <div class="justify content-center d-flex flex-column" style="width:90%; margin-left:5%; margin-right:5%">
+            <label class="mx-auto form-control is-invalid fs-5" for="floatingInputInvalid"> Você não pode acessar esta página porque não está logado.</label>
+            <br>
+            <a href="login.php" style="margin:auto;background-color: #1A4A65" class="btn fs-4 text-white" role="button">Entrar</a></p>
+        </div>
+
+
+    <?php
+        die();
+    }
+
+    ?>
+</body>
